81439c3871f2104cd43573c7d606bd38cbb29a07
Phil Van-Lane <philv19@gmail.com>
2025-11-14 15:21:57 +0100

    streamlined structure

--- convert_md_to_html.sh
+++ convert_md_to_html.sh
@@ -2,7 +2,7 @@ pandoc markdown/index.md \
   --from markdown \
   --to html5 \
   --standalone \
-  --toc --toc-depth=2 \
+  --toc --toc-depth=3 \
   --template html-template/template.html \
   --metadata title="Graduate Student Handbook" \
   --output index.html
